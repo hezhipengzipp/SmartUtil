3274d1ed90f39b89705bcbc098c6e15109562660
贺志鹏 <zippsun@126.com>
2024-09-17 14:49:15 +0800

    feat：增加dokit开发工具。

--- app/src/main/java/com/clife/smartutil/MyApplication.kt
+++ app/src/main/java/com/clife/smartutil/MyApplication.kt
@@ -2,6 +2,7 @@ package com.clife.smartutil
 
 import android.app.Application
 import android.util.Log
+import com.didichuxing.doraemonkit.DoKit
 
 import com.github.anrwatchdog.ANRWatchDog
 
@@ -9,6 +10,8 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         ANRWatchDog().start()
+        DoKit.Builder(this)
+            .build()
         Log.e("MyApplication", "onCreate")
     }
 }
